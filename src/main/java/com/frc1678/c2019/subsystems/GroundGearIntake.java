94a5b7a54e4a4538558375dbe2aa7c733d7857a9
jonathanhoNumberXIII <jonathanbho@gmail.com>
2020-01-02 16:04:56 -0800

    Ground Gear Intake 2.0 By Jonathan H.
    Updated the code so it doesn't cause much confusion for the other people
    who view it.

--- src/main/java/com/frc1678/c2019/subsystems/GroundGearIntake.java
+++ src/main/java/com/frc1678/c2019/subsystems/GroundGearIntake.java
@@ -33,21 +33,23 @@ public class GroundGearIntake extends Subsystem {
     // Constants, make sure that you have public static before each of them
     public static double kVoltIntake = -7.0;
     public static double kVoltOut = -3.0;
+    public static double kVoltHold = -4.0;
+    public static double kVoltCarry = -5.0;
 
     private static GroundGearIntake mInstance;
 
 
     public enum WantedAction {
         // Insert the action
-        NONE, SCORE, DROP, RISE, OUTTAKE, START_BALLDROP, STOP_BALLDROP
+        NONE, SCORE, DROP, RISE, INTAKE, OUTTAKE, START_BALLDROP, STOP_BALLDROP
     }
 
     private enum State {
         // Different types of states
-        NONE, IDLE, INTAKE, PICKUP, CARRY, SCORING, OUTTAKE, BALLDROP_GEAR, BALLDROP_NOGEAR
+        IDLE, INTAKE, PICKUP, CARRY, SCORING, OUTTAKE, BALLDROP_GEAR, BALLDROP_NOGEAR
     }
 
-    private State mState = State.NONE;
+    private State mState = State.IDLE;
 
     // Any private variables you may need
     
@@ -63,8 +65,8 @@ public class GroundGearIntake extends Subsystem {
     private GroundGearIntake() {
      // Set each actuator to their ID's 
     
-    mMaster = TalonSRXFactory.createDefaultTalon(Constants.kCargoIntakeRollerId);
-    mGearIntSolenoid = Constants.makeSolenoidForId(Constants.kCargoIntakePopoutSolenoidId);
+        mMaster = TalonSRXFactory.createDefaultTalon(Constants.kCargoIntakeRollerId);
+        mGearIntSolenoid = Constants.makeSolenoidForId(Constants.kCargoIntakePopoutSolenoidId);
 
     }
 
@@ -97,7 +99,7 @@ public class GroundGearIntake extends Subsystem {
         enabledLooper.register(new Loop() {
             @Override
             public void onStart(double timestamp) {
-                mState =  State.IDLE;
+                mState = State.IDLE;
                 // startLogging();
             }
 
@@ -114,40 +116,38 @@ public class GroundGearIntake extends Subsystem {
                 mState = State.IDLE;
                 stopLogging();
             }
-        });
+        }
+        );
     }
 
 
     public void runStateMachine() {
         switch (mState) {
         // Cases for each state and what the actuators should be at those states
-        case NONE:
-            mPeriodicIO.GearIntSolenoid = false;
-            mPeriodicIO.demand = 0;
-        case IDLE:
-            mPeriodicIO.GearIntSolenoid = false;
-            mPeriodicIO.demand = 0;
-        case INTAKE:
-            mPeriodicIO.GearIntSolenoid = true;
-            mPeriodicIO.demand = kVoltIntake;
-        case PICKUP:
-            mPeriodicIO.GearIntSolenoid = false;
-            mPeriodicIO.demand = kVoltIntake;
-        case CARRY:
-            mPeriodicIO.GearIntSolenoid = false;
-            mPeriodicIO.demand = 0;
-        case SCORING:
-            mPeriodicIO.GearIntSolenoid = false;
-            mPeriodicIO.demand = kVoltOut;
-        case OUTTAKE:
-            mPeriodicIO.GearIntSolenoid = false;
-            mPeriodicIO.demand = kVoltOut;
-        case BALLDROP_GEAR:
-            mPeriodicIO.GearIntSolenoid = true;
-            mPeriodicIO.demand = 0;
-        case BALLDROP_NOGEAR:
-            mPeriodicIO.GearIntSolenoid = false;
-            mPeriodicIO.demand = 0;
+            case IDLE:
+                mPeriodicIO.GearIntSolenoid = false;
+                mPeriodicIO.demand = 0;
+            case INTAKE:
+                mPeriodicIO.GearIntSolenoid = true;
+                mPeriodicIO.demand = kVoltIntake;
+            case PICKUP:
+                mPeriodicIO.GearIntSolenoid = false;
+                mPeriodicIO.demand = kVoltIntake;
+            case CARRY:
+                mPeriodicIO.GearIntSolenoid = false;
+                mPeriodicIO.demand = 0;
+            case SCORING:
+                mPeriodicIO.GearIntSolenoid = false;
+                mPeriodicIO.demand = kVoltOut;
+            case OUTTAKE:
+                mPeriodicIO.GearIntSolenoid = false;
+                mPeriodicIO.demand = kVoltOut;
+            case BALLDROP_GEAR:
+                mPeriodicIO.GearIntSolenoid = true;
+                mPeriodicIO.demand = 0;
+            case BALLDROP_NOGEAR:
+                mPeriodicIO.GearIntSolenoid = false;
+                mPeriodicIO.demand = 0;
 
         }
     }
@@ -156,44 +156,44 @@ public class GroundGearIntake extends Subsystem {
     public void setState(WantedAction wanted_state) {
         switch (wanted_state) {
         // Cases to switch 
-        case DROP:
-          mState = State.INTAKE;
-          break;
-        case RISE:
-          if (mState == State.INTAKE) {
-            mState = State.IDLE;
-          }
-          break;
-        case SCORE:
-          if (mState == State.CARRY || mState == State.IDLE) {
-            mState = State.SCORING;
-          }
-          break;
-        case NONE:
-          if (mState == State.SCORING) {
-            mState = State.IDLE;
-          }
-          break;
-        case OUTTAKE:
-          mState = State.OUTTAKE;
-          break;
-        case START_BALLDROP:
-          if (mState == State.PICKUP || mState == State.CARRY) {
-            mState = State.BALLDROP_GEAR;
-          }
-          if (mState == State.INTAKE || mState == State.IDLE) {
-            mState = State.BALLDROP_NOGEAR;
-          }
-          break;
-        case STOP_BALLDROP:
-          if (mState == State.BALLDROP_NOGEAR) {
-            mState = State.IDLE;
-          }
-          if (mState == State.BALLDROP_NOGEAR) {
-            mState = State.CARRY;
-          }
-          break;
-            }
+            case DROP:
+              mState = State.INTAKE;
+              break;
+            case RISE:
+              if (mState == State.INTAKE) {
+                mState = State.IDLE;
+              }
+              break;
+            case SCORE:
+              if (mState == State.CARRY || mState == State.IDLE) {
+                mState = State.SCORING;
+              }
+              break;
+            case NONE:
+              if (mState == State.SCORING) {
+                mState = State.IDLE;
+              }
+              break;
+            case OUTTAKE:
+              mState = State.OUTTAKE;
+              break;
+            case START_BALLDROP:
+              if (mState == State.PICKUP || mState == State.CARRY) {
+                mState = State.BALLDROP_GEAR;
+              }
+              if (mState == State.INTAKE || mState == State.IDLE) {
+                mState = State.BALLDROP_NOGEAR;
+              }
+              break;
+            case STOP_BALLDROP:
+              if (mState == State.BALLDROP_NOGEAR) {
+                mState = State.IDLE;
+              }
+              if (mState == State.BALLDROP_NOGEAR) {
+                mState = State.CARRY;
+              }
+              break;
+        }
     }
 
     @Override
